7f53caa1b43a3a14e27ad263e3d84ddaace86908
Nicoogf <nicolasgfalabella@gmail.com>
2023-06-12 14:48:55 -0300

    Configuracion del modo Dark

--- client/src/index.js
+++ client/src/index.js
@@ -15,14 +15,14 @@ import {
   PURGE,
   REGISTER
 } from "redux-persist" ;
-import { storage } from 'redux-persist/lib/storage';
+import storage  from 'redux-persist/lib/storage';
 import { PersistGate } from 'redux-persist/integration/react';
 
 
 const persistConfig = { key : "root" , storage ,  version:1} ;
 const persistedReducer = persistReducer( persistConfig , authReducer ) ;
 const store = configureStore ({
-  reducer : persistReducer ,
+  reducer : persistedReducer ,
   middleware: (getDefaultMidleware) =>{
     getDefaultMidleware({
       serializableCheck:[FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
