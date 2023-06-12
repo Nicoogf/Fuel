c4a85e4b4907f64c840c7f7fd9e56f74b57d4d26
Nicoogf <nicolasgfalabella@gmail.com>
2023-06-12 15:22:38 -0300

    Se avanza sobre el diseño del Nav

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -27,8 +27,26 @@ import {
 
 
 const Navbar = () =>{
-    return(
-        <div> navbar </div>
+    const  [isMobileMenuToggled , setidmobileMenuToggled] = useState(false);
+    const  dispactch = useDispatch();
+    const  navigate = useNavigate();
+    const  user = useSelector( (state) => state.user ); 
+    const isNonMobileScreens = useMediaQuery( "(min-width:1000px)" ) ;
+    
+    const theme = useTheme ();
+    const neutralLight =theme.palette.neutral.ligth ; 
+    const dark = theme.palette.neutral.dark ;
+    const background = theme.palette.background.default ; 
+    const primary =  theme.palette.primary.light ;
+    const alt = theme.palette.background.alt ;
+
+    const fullName = `${user.firstName} ${user.lastName}` ;
+
+    return( 
+
+        <Flexbetween padding="1rem 6%" background={alt}>
+
+        </Flexbetween>
     )
 }
 
