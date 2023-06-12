b92ed332dab6824b261ed339d8edcd65feea2fac
Nicoogf <nicolasgfalabella@gmail.com>
2023-06-11 21:53:51 -0300

    Se terminan las rutas del Post

--- server/index.js
+++ server/index.js
@@ -9,8 +9,11 @@ import morgan from "morgan" ;
 import path from "path" ;
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js" ;
-import userRouter  from "./routes/users.js"
+import userRouter  from "./routes/users.js";
+import postRouter  from "./routes/posts.js";
 import { register } from "./controllers/auth.js" ;
+import { createPost } from "./controllers/posts.js" ;
+import { verifyToken } from "./middleware/auth.js";
 
 
 /* Configuracion */
@@ -46,11 +49,13 @@ const upload = multer({storage}) ;
 /*Rutas con Archivos */
 
 app.post( "/auth/register" , upload.single("picture")  , register );
+app.post ("/posts" , verifyToken  , upload.single("picture"), createPost ) ;
 
 /*Rutas*/
 
 app.use("/auth" , authRoutes) ;
 app.use("/users" , userRouter) ;
+app.use("/posts" , postRouter ) ;
 
 
 /* Mongoose DB Configuracion */
