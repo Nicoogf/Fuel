5fc7810a4bdbd7f430986797e75a838ade789e4e
Nicoogf <nicolasgfalabella@gmail.com>
2023-06-13 13:12:58 -0300

    Registro,loguin y formularios

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -27,3 +27,70 @@ const registerSchema = yup.object().shape({
     ocupation: yup.string().required("required"),
     picture: yup.string().required("required"),
 })
+
+const loginSchema = yup.object().shape({
+    email : yup.string().email("Email Invalido").required("required"),
+    password : yup.string().required("required"),
+});
+
+const initalValuesRegister = {
+    firstName :"",
+    lastName : "",
+    email: "",
+    password: "",
+    localtion: "",
+    ocupation: "" ,
+    picture:"",
+}
+
+const initialValuesLogin ={
+    email: "",
+    password: "",
+};
+
+const Form = () =>{
+    const[pageType, setPageType] = useState("login");
+    const { palette } = useTheme();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const isNonMobile = useMediaQuery("min-width:600px");
+    const isLogin = pageType === "login ";
+    const isRegister = pageType === "register" ;
+
+    const handleFormsSubmit = async ( values , onsubmit) =>{
+        return (
+            <Formik onSubmit={handleFormsSubmit}
+                    initialValues={isLogin ? initialValuesLogin : initalValuesRegister}
+                    validationSchema={isLogin ? loginSchema :  registerSchema}
+            >
+
+            {({
+                values,
+                errors,
+                touched,
+                handleBlur,
+                handleChange,
+                handleSubmit,
+                setFieldValue,
+                resetForm,
+            }) = (
+                <form onSubmit={handleSubmit}>
+                    <Box display="grid"
+                         gap="30px" 
+                         gridTemplateColumns= "repeat(4, minmax(0, 1fr))"
+                         sx={{
+                            "& > div": {gridColumn : isNonMobile ? undefined : "span 4"},
+                         }}
+                   >
+
+                    </Box>
+                </form>
+            )}
+
+            </Formik>
+        )
+    }
+}
+
+
+export default Form ;
